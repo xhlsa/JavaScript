cbb14fb53f1db9d6cf012fcf386d8612767931fd
xhlsa <sasseenh@gmail.com>
2023-12-01 15:26:31 -0700

    Speed counter and better control inside circle.

--- script.js
+++ script.js
@@ -41,6 +41,9 @@ function getRandomColor() {
 }
 
 function update() {
+    const acceleration = 0.01;
+    const dampingFactor = 0.02;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBoundary();
 
@@ -50,27 +53,48 @@ function update() {
     ball.x += ball.vx;
     ball.y += ball.vy;
 
+    // Calculate speed for the speed display
+    let speed = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+
     // Check collision with the circular boundary
     let dx = ball.x - circleBoundary.x;
     let dy = ball.y - circleBoundary.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
     if (distance + ball.radius > circleBoundary.radius) {
+        // Adjust the ball's position to be exactly on the boundary
+        let overlap = distance + ball.radius - circleBoundary.radius;
+        ball.x -= overlap * (dx / distance);
+        ball.y -= overlap * (dy / distance);
+
+        // Reflect the ball off the boundary
         let angle = Math.atan2(dy, dx);
-        ball.vx = -Math.cos(angle) * Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
-        ball.vy = -Math.sin(angle) * Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+        let newSpeed = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+        ball.vx = -Math.cos(angle) * newSpeed;
+        ball.vy = -Math.sin(angle) * newSpeed;
 
         ball.color = getRandomColor();
 
-        // Diminishing increase in velocity
-        let speed = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
-        let increaseFactor = 1.02 + (1 / speed);
-        if (increaseFactor > 1.05) increaseFactor = 1.05; // Cap the maximum increase
-
-        ball.vx *= increaseFactor;
-        ball.vy *= increaseFactor;
+        // Adjust the velocity based on acceleration and damping
+        let increase = acceleration - (dampingFactor * newSpeed);
+        ball.vx += increase * Math.cos(angle);
+        ball.vy += increase * Math.sin(angle);
     }
+
+    drawSpeed(speed); // Display the speed
 
     requestAnimationFrame(update);
 }
+
+
+
+function drawSpeed(speed) {
+    ctx.save();
+    ctx.font = '16px Arial';
+    ctx.fillStyle = 'white';
+    ctx.fillText(`Speed: ${speed.toFixed(2)}`, 10, canvas.height - 10);
+    ctx.restore();
+}
+
+
 
 update();
